05aa9bf83517e9e57e773c3cf76bde90f442f469
Himni Allen <himni.allen@students.snow.edu>
2025-04-12 21:46:33 -0600

    enimies made as far as without items can be done.

--- enemies.cs
+++ enemies.cs
@@ -1,5 +1,7 @@
 // in Attack methods, weapon damage isn't caculated yet. // to do after weapons implemented.
 
+using System.Drawing;
+
 namespace hhDungeon;
 
 
@@ -80,7 +82,7 @@ public class Goblin : Enimies
         {
             diff_level = max_diff_level;
         }
-        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth)*difficulty/3;
+        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth) * difficulty / 3;
     }
 
     public int GrabGold(int difficulty, int player_level)
@@ -121,7 +123,7 @@ public class Slime : Enimies
         {
             diff_level = max_diff_level;
         }
-        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth + diff_level)*difficulty/5;
+        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth + diff_level) * difficulty / 5;
     }
 
     public int GrabGold(int difficulty, int player_level)
@@ -132,10 +134,201 @@ public class Slime : Enimies
             diff_level = max_diff_level;
         }
 
-        return (difficulty * rnd.Next(min_gold, max_gold +1) + diff_level);
+        return (difficulty * rnd.Next(min_gold, max_gold + 1) + diff_level);
     }
 
 }
+
+
+public class Orc : Enimies
+{
+
+    //### Orc
+
+    //    Orcs are goblins big brothers.They're bigger, tougher, and harder to beat. They usually carry a club or sword, and instead of ever being unarmed, they always have at LEAST a dagger. Somewhat harder to beat, less frequent to spawn.
+    //##### Damage
+    //    ((1/(6-9) * maxPlayerHP) + base weapon damage) * floor/4
+    //##### Gold range
+    //    ((dif + 1) * (1 - 4)) + (floor* player level)capped at 20
+    //##### Item pool:
+    //- Club
+    //- Dagger
+    //- Sword
+    //- chain armor
+    int min_damage_rel_player = 9;
+    int max_damage_rel_player = 6;
+    Random rnd = new();
+    int max_diff_level = 20;
+    int min_gold = 1;
+    int max_gold = 4;
+    public override int Attack(int player_maxhealth, int player_level, int difficulty) // returns the damage.
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth + diff_level) * difficulty / 4;
+    }
+
+    public int GrabGold(int difficulty, int player_level)
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+
+        return (difficulty * rnd.Next(min_gold, max_gold + 1) + diff_level);
+    }
+
+}
+
+public class Troll : Enimies
+{
+
+    //### Troll
+
+    //    BIG OL THICCUMS.They're much bigger, and much harder to beat than Goblins or Orcs. Also generally carrying a club. If in a room, are the only ones there.
+    //##### Damage
+    //    ((1/(3-6) * maxPlayerHP) + base weapon damage) * floor/5
+    //##### Gold range
+    //    ((dif + 1) * (0 - 7)) + (floor* player level)capped at 30
+    //##### Item pool:
+    //- Club
+    //- Sword
+    //- +50% max health
+    //- +(n%)strength for (3-5) rooms
+    //- +(n%)defense for (3-5) rooms
+    //- permanant +(n%) strength or defense
+
+    int min_damage_rel_player = 6;
+    int max_damage_rel_player = 3;
+    Random rnd = new();
+    int max_diff_level = 30;
+    int min_gold = 0;
+    int max_gold = 7;
+    public override int Attack(int player_maxhealth, int player_level, int difficulty) // returns the damage.
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth + diff_level) * difficulty / 5;
+    }
+
+    public int GrabGold(int difficulty, int player_level)
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+
+        return (difficulty * rnd.Next(min_gold, max_gold + 1) + diff_level);
+    }
+
+}
+
+
+
+
+public class Skeleton : Enimies
+{
+
+    //### Skeleton
+
+    //    More similar to goblins in difficulty and frequency, generally armed with their own ribs or something similar, maybe a chain.
+    //##### Damage
+    //    ((1/(7-12) * maxPlayerHP) + base weapon damage) * floor/3
+    //##### Gold range
+    //    ((dif + 1) * (0 - 2)) + (floor* player level)capped at 15
+    //##### Item pool:
+    //- Rib
+    //- +15 health potion
+    //- nicer armor(tbd)
+    //- +(n%) strength or defence potion
+
+    int min_damage_rel_player = 12;
+    int max_damage_rel_player = 7;
+    Random rnd = new();
+    int max_diff_level = 15;
+    int min_gold = 0;
+    int max_gold = 2;
+    public override int Attack(int player_maxhealth, int player_level, int difficulty) // returns the damage.
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth + diff_level) * difficulty / 3;
+    }
+
+    public int GrabGold(int difficulty, int player_level)
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+
+        return (difficulty * rnd.Next(min_gold, max_gold + 1) + diff_level);
+    }
+
+}
+
+
+public class Dragon : Enimies
+{
+
+    //### Dragon
+
+    //    More similar to goblins in difficulty and frequency, generally armed with their own ribs or something similar, maybe a chain.
+    //##### Damage
+    //    ((1/(3-5) * maxPlayerHP) + base weapon damage) * floor/10
+    //##### Gold range
+    //    ((dif + 1) * (15 - 80)) + (floor* player level)capped at 20
+    //##### Item pool:
+    //- Rib
+    //- +15 health potion
+    //- nicer armor(tbd)
+    //- +(n%) strength or defence potion
+
+    int min_damage_rel_player = 5;
+    int max_damage_rel_player = 3;
+    Random rnd = new();
+    int max_diff_level = 20;
+    int min_gold = 15;
+    int max_gold = 80;
+    public override int Attack(int player_maxhealth, int player_level, int difficulty) // returns the damage.
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+        return (rnd.Next(max_damage_rel_player, min_damage_rel_player + 1) * player_maxhealth + diff_level) * difficulty / 10;
+    }
+
+    public int GrabGold(int difficulty, int player_level)
+    {
+        int diff_level = difficulty * player_level;
+        if (diff_level > max_diff_level)
+        {
+            diff_level = max_diff_level;
+        }
+
+        return (difficulty * rnd.Next(min_gold, max_gold + 1) + diff_level);
+    }
+
+}
+
+
+
+
+
 
 // Fields:
 // - +Difficulty : int // used in constructor to determine other fields.
