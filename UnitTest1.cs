3a1d043ac67ccaf1d59a718eda6f7a3b71ba699b
dharryc <dharrisoncook@gmail.com>
2025-04-25 10:26:28 -0600

    HARD CODED

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,7 +7,19 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-        Program.GreetPlayer();
+        List<Room> rooms = [];
+        for (int i = 0; i < 200; i++)
+        {
+            rooms.Add(new Room(1, false, false));
+        }
+        foreach (var i in rooms) Console.WriteLine(Convert.ToString(i.type));
+        foreach (var i in rooms)
+        {
+            if (i.type == RoomType.store)
+            {
+                foreach (var b in i.storeCosts) Program.DisplayInventoryItem(b.item, 1);
+            }
+        }
     }
 
     [Test]
